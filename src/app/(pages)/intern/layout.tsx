3b599bd4e60125486a6e9723c502614a9a3b1067
Martin Holtschneider <code@martin-holtschneider.de>
2024-11-05 23:54:56 +0100

    dynamic rendering of pages in the logged in state

--- src/app/(pages)/intern/layout.tsx
+++ src/app/(pages)/intern/layout.tsx
@@ -3,6 +3,12 @@ import { AuthProvider } from '@/next/auth/context';
 
 import Loading from './loading';
 
+/*
+ * interestingly the 'force-dynamic' is needed because the rendering of all interal pages is dependent on the login state of the user.
+ * Without 'force-dynamic' NEXTjs generates static pages that are empty because they only render content when the user is logged in.
+ */
+export const dynamic = 'force-dynamic';
+
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
   return (
     <AuthProvider>
