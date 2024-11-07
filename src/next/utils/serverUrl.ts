b69691a9879a2923c727b7dff2f524f9167e24fe
Martin Holtschneider <code@martin-holtschneider.de>
2024-11-08 00:50:04 +0100

    SERVER_URL for client side usage

--- src/next/utils/serverUrl.ts
+++ src/next/utils/serverUrl.ts
@@ -1,5 +1,5 @@
-export const SERVER_URL = process.env.LOCAL_SERVER_URL_OVERRIDE
-  ? process.env.LOCEL_SERVER_URL_OVERRIDE
-  : process.env.VERCEL_ENV === 'production'
-    ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
-    : `https://${process.env.VERCEL_BRANCH_URL}`;
+export const SERVER_URL = process.env.NEXT_PUBLIC_LOCAL_SERVER_URL_OVERRIDE
+  ? process.env.NEXT_PUBLIC_LOCAL_SERVER_URL_OVERRIDE
+  : process.env.NEXT_PUBLIC_VERCEL_ENV === 'production'
+    ? `https://${process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL}`
+    : `https://${process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL}`;
