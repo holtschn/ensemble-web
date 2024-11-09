7281314bf69b9f165106a1bd07e12740bd267223
Martin Holtschneider <code@martin-holtschneider.de>
2024-11-09 13:15:22 +0100

    header accomodates different logo aspect ratios

--- src/next/components/header.client.tsx
+++ src/next/components/header.client.tsx
@@ -13,13 +13,24 @@ export type HeaderComponentClientProps = {
 
 export const HeaderComponentClient: React.FC<HeaderComponentClientProps> = ({ links, logo }) => {
   const { animateHeaderOnScroll } = useAnimation();
+  /*
+   * We want the logo to be within 120x30 pixels keeping its aspect ratio.
+   * ...but we do not know which dimension hits the boundary first.
+   */
+  let computedLogoHeight = 30;
+  let computedLogoWidth = ((logo?.width ?? 0) / (logo?.height ?? 1)) * computedLogoHeight;
+  if (computedLogoWidth > 120) {
+    computedLogoWidth = 120;
+    computedLogoHeight = ((logo?.height ?? 0) / (logo?.width ?? 1)) * computedLogoWidth;
+  }
+
   return (
     <header
       className={`fixed top-0 z-50 w-full bg-white ${animateHeaderOnScroll ? 'animate-appear-scroll' : 'animate-appear'}`}
     >
       <nav className="flex justify-between items-center py-2 mx-4 middle-column">
         <Link href="/">
-          <Image src={logo?.url ?? ''} alt={logo?.alt ?? ''} height={30} width={90} />
+          <Image src={logo?.url ?? ''} alt={logo?.alt ?? ''} height={computedLogoHeight} width={computedLogoWidth} />
         </Link>
         {links && links.length > 0 && (
           <ul className="flex space-x-4">
