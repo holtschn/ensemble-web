8ae4ff7d9011c3eb6aacde5a457a2f4f459fb2ab
Martin Holtschneider <code@martin-holtschneider.de>
2025-07-04 23:55:05 +0200

    optimized table search bar layout

---
+++ src/next/ndb/components/table/FilterButton.tsx
@@ -0,0 +1,22 @@
+import Button from '@/next/ndb/components/form/Button';
+import Image from 'next/image';
+import React from 'react';
+
+type FilterButtonProps = {
+  isActive: boolean;
+  onClick: () => void;
+  children: React.ReactNode;
+};
+
+export const FilterButton: React.FC<FilterButtonProps> = ({ isActive, onClick, children }) => {
+  return (
+    <Button size="sm" className="text-xs" variant={isActive ? 'primary' : 'outline'} onClick={onClick}>
+      {isActive ? (
+        <Image src="/filter-active.svg" alt="Filter Active Icon" width={8} height={8} className="mr-2 h-3 w-3" />
+      ) : (
+        <Image src="/filter-inactive.svg" alt="Filter Inactive Icon" width={8} height={8} className="mr-2 h-3 w-3" />
+      )}
+      {children}
+    </Button>
+  );
+};
